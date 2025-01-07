b8a6bf2b57554c6829949d7ff01c7dfd5f37e589
Stuart Forrester <stforrester@gmail.com>
2025-01-07 16:30:12 -0600

    completes lab and passes tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, anagram_list):
+        anagrams = []
+        for item in anagram_list:
+            item_list = list(item)
+            if sorted(item_list) == sorted(self.word):
+                anagrams.append(item)
+        return anagrams
